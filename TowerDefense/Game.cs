13bbb099e9a818a106520599955748ab9d24d6f9
Sanj Prem <sanj.commited@gmail.com>
2018-02-27 13:23:09 -0500

    Using auto-properties to create invader paths

--- TowerDefense/Game.cs
+++ TowerDefense/Game.cs
@@ -27,12 +27,6 @@ namespace TowerDefense
                         new MapLocation(7, 2, map)
                     }
                 );
-                MapLocation location = path.GetLocationAt(8);
-
-                if(location != null)
-                {
-                    Console.WriteLine(location.X + "," + location.Y);
-                }
 
             }
             catch(OutOfBoundsException ex)
